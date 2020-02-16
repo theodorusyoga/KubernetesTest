28de54fc1d78d427ceee288a74748824b9a20775
Theodorus Yoga <theoyoga@outlook.com>
2020-02-16 15:18:23 +0700

    Add mysql connection using docker compose. Add db config

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express')
+const mysql = require('mysql')
 const app = express()
 const port = 3000
 
 const env = process.env.NODE_ENV || 'DEFAULT'
+const sqlPass = process.env.MYSQL_PASSWORD
 
 app.get('/hello', (req, res) => res.send('Hello World!'))
 
-app.get('/environment', (req, res) => res.send(`Your environment is: ${env}`))
+app.get('/environment', (req, res) => {
+    return res.send(`Your environment is: ${env}`)
+})
+
+app.get('/dbtest', (req, res) => {
+    const connection = mysql.createConnection({
+        host: 'mysql',
+        user: 'root',
+        password: sqlPass,
+        database: 'kubernetes_test',
+        port: 3306
+    })
+    connection.connect((err) => {
+        if(err) {
+            return res.status(400).send('MySQL connection error')
+        } else {
+            return res.send('MySQL connection success!')
+        }
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
